3e90811eda2d8908356f61362527fc1fcc09968b
ino8090 <ibrahirahimhalil@gmail.com>
2026-09-01 23:13:40 +0300

    Update logo scaling and overlay parameters
    Adjust logo scaling and overlay position in video processing.

--- maxdeneme.py
+++ maxdeneme.py
@@ -233,8 +233,8 @@ def start_m3u_stream():
             filter_str = (
                 '[0:v]scale=1920:1080:force_original_aspect_ratio=decrease,'
                 'pad=1920:1080:(ow-iw)/2:(oh-ih)/2:black,fps=30[main];'
-                f'[{logo_input_index}:v]scale=-2:55,format=rgba,colorchannelmixer=aa={logo_alpha}[logo];'
-                '[main][logo]overlay=main_w-overlay_w-79:79[tmp];'
+                f'[{logo_input_index}:v]scale=-2:60,format=rgba,colorchannelmixer=aa={logo_alpha}[logo];'
+                '[main][logo]overlay=main_w-overlay_w-83:83[tmp];'
                 f'[tmp]{drawtext_filter}'
             )
         else:
