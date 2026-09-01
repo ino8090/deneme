f724eb7b2742c19a5223fe8916d1797c8e13c268
ino8090 <ibrahirahimhalil@gmail.com>
2026-09-02 01:12:50 +0300

    Adjust logo scaling and overlay positioning

--- maxdeneme.py
+++ maxdeneme.py
@@ -257,8 +257,8 @@ def start_m3u_stream():
             filter_str = (
                 '[0:v]scale=1920:1080:force_original_aspect_ratio=decrease,'
                 'pad=1920:1080:(ow-iw)/2:(oh-ih)/2:black,fps=30[main];'
-                f'[{logo_input_index}:v]scale=-2:80,format=rgba,colorchannelmixer=aa={logo_alpha}[logo];'
-                '[main][logo]overlay=main_w-overlay_w-98:98[tmp];'
+                f'[{logo_input_index}:v]scale=-2:88,format=rgba,colorchannelmixer=aa={logo_alpha}[logo];'
+                '[main][logo]overlay=main_w-overlay_w-109:98[tmp];'
                 f'[tmp]{drawtext_filter}'
             )
         else:
