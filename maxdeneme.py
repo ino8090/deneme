4c1cb19f3434086c67a5615c7fb52f35f85de123
ino8090 <ibrahirahimhalil@gmail.com>
2026-09-05 01:10:15 +0300

    Adjust logo overlay position in maxdeneme.py

--- maxdeneme.py
+++ maxdeneme.py
@@ -257,7 +257,7 @@ def start_m3u_stream():
                 'pad=1920:1080:(ow-iw)/2:(oh-ih)/2:black,fps=25[main];'
                 f'[{logo1_input_index}:v]scale=-2:109,format=rgba,'
                 f'colorchannelmixer=aa={LOGO_OPACITY}[logo1];'
-                '[main][logo1]overlay=main_w-overlay_w-90:59[tmp];'
+                '[main][logo1]overlay=main_w-overlay_w-110:100[tmp];'
                 f'[tmp]{title_drawtext}[v]'
             )
         else:
