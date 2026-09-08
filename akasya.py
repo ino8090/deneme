4f79c3d9c73e05e884a0b945ab4780f0ada0d6b8
ino8090 <ibrahirahimhalil@gmail.com>
2026-09-08 23:38:37 +0300

    Adjust logo and title text positioning in akasya.py

--- akasya.py
+++ akasya.py
@@ -237,7 +237,7 @@ def start_m3u_stream():
         title_drawtext = (
             f"drawtext=textfile='title.txt':reload=1:fontfile='{BOLD_FONT_PATH}':"
             f"fontcolor=white@{TEXT_OPACITY}:fontsize=28:"
-            f"x=80:y=main_h-th-50"
+            f"x=50:y=main_h-th-50"
         )
 
         if has_logo1:
@@ -247,7 +247,7 @@ def start_m3u_stream():
                 'pad=1920:1080:(ow-iw)/2:(oh-ih)/2:black,fps=25[main];'
                 f'[{logo1_input_index}:v]scale=-2:98,format=rgba,'
                 f'colorchannelmixer=aa={LOGO_OPACITY}[logo1];'
-                '[main][logo1]overlay=main_w-overlay_w-113:50[tmp];'
+                '[main][logo1]overlay=main_w-overlay_w-50:50[tmp];'
                 f'[tmp]{title_drawtext}[v]'
             )
         else:
